2aa6972aa2774f0c4c50fd19c6ddd300bd429dea
Timmeyontour <timgleibs@gmx.de>
2024-10-24 20:55:02 +0200

    fixed display.flip

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import player
 from constants import *
 
 def main():
@@ -10,16 +11,22 @@ def main():
     print(f"Screen width: {SCREEN_HEIGHT}")
     # https://www.pygame.org/docs/ref/pygame.html#pygame.init
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     # Use pygame's display.set_mode() to get a new GUI window:
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game_loop(screen)
+    player1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    game_loop(screen, clock, dt, player1)
 
-def game_loop(screen):
+def game_loop(screen, clock, dt, player1):
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0,0,0), rect=None, special_flags=0)
+        player1.draw(screen)
+        dt = (clock.tick(60)/1000)
         pygame.display.flip()
 
 # This line ensures the main() function is only called when this file is run directly; 
---
+++ player.py
@@ -0,0 +1,25 @@
+import pygame
+import circleshape
+from constants import PLAYER_RADIUS
+
+class Player(circleshape.CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def draw(self, screen):
+        # sub-classes must override
+        pygame.draw.polygon(screen, (255,255,255), self.triangle(), 2)
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
+
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
